c89d50c8df394f2ceab9895d11d166c92365cbf4
Thomas Dillard <thomas.dillard.studios@gmail.com>
2018-12-17 23:27:43 -0700

    working webpack config.

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,24 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const ROOT_DIR = path.resolve(__dirname);
 
 module.exports = {
   mode: 'development',
-  entry: {
-    app: path.resolve(ROOT_DIR, './src/index.html'),
-    // print: './src/print.js'
-  },
+  entry: path.resolve(ROOT_DIR, './src/index.js'),
   devServer: {
-    // contentBase: path.resolve(ROOT_DIR, './src/index.html'),
-    // compress: true,
     port: 3000,
   },
-  plugins: [new HtmlWebpackPlugin()]
+  module: {
+    rules: [{
+      test: /\.html$/,
+      use: [{
+        loader: 'html-loader',
+        options: {
+          minimize: true,
+          removeComments: false,
+          collapseWhitespace: false,
+        },
+      }],
+    }],
+  },
 };
-

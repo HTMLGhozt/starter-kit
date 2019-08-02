7ab9bde4296626748eb070ecc80ae28c0922efb5
Thomas Dillard <thomas.dillard.studios@gmail.com>
2019-08-02 01:59:19 -0400

    end of day commit

---
+++ client/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+const ROOT_DIR = path.resolve(__dirname);
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(ROOT_DIR, './src/index.js'),
+  devServer: {
+    port: 3000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: {
+              minimize: true,
+              removeComments: false,
+              collapseWhitespace: false,
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
